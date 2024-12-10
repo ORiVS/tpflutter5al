188194d48c896d276443800d2e406f7950339c77
ORiVS <107984700+ORiVS@users.noreply.github.com>
2024-12-10 14:02:57 +0100

    ajout de flutter bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:tpflutter5al/posts_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'posts_bloc/posts_bloc.dart';
+import 'package:tpflutter5al/data/posts_data_source.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    BlocProvider(
+      create: (context) => PostsBloc(FakePostsDataSource()), // Injectez ici votre bloc et la source de données
+      child: const MyApp(),
+    ),
+  );
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/posts_screen.dart
+++ lib/posts_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'posts_bloc/posts_bloc.dart';
+import '../models/post.dart';
 
 class PostsScreen extends StatelessWidget {
   const PostsScreen({super.key});
@@ -9,6 +13,44 @@ class PostsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Posts'),
       ),
+      body: BlocBuilder<PostsBloc, PostsState>(
+        builder: (context, state) {
+          // Vérifier l'état des posts
+          if (state is PostsLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is PostsErrorState) {
+            return Center(child: Text(state.message));
+          } else if (state is PostsLoadedState) {
+            final posts = state.posts; // Récupération de la liste des posts
+
+            return RefreshIndicator(
+              onRefresh: () async => _getAllPosts(context), // Fonction de rafraîchissement
+              child: ListView.separated(
+                itemCount: posts.length,
+                separatorBuilder: (context, index) => const SizedBox(height: 20),
+                itemBuilder: (context, index) {
+                  final post = posts[index];
+                  return ListTile(
+                    title: Text(post.title),
+                    subtitle: Text(post.description),
+                    onTap: () {
+                      // Naviguer vers une page de détails ou d'édition de post si nécessaire
+                    },
+                  );
+                },
+              ),
+            );
+          } else {
+            return const Center(child: Text('No posts available'));
+          }
+        },
+      ),
     );
+  }
+
+  // Fonction pour récupérer tous les posts
+  void _getAllPosts(BuildContext context) {
+    final postsBloc = BlocProvider.of<PostsBloc>(context);
+    postsBloc.add(const GetAllPosts());
   }
 }
